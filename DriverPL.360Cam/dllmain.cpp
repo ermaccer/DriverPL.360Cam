f3626ad762cb30ff4512e0fc143b7ede23703118
ermaccer <40604575+ermaccer@users.noreply.github.com>
2023-03-17 22:05:01 +0100

    negotiator fix

--- DriverPL.360Cam/dllmain.cpp
+++ DriverPL.360Cam/dllmain.cpp
@@ -2,14 +2,42 @@
 #include "pch.h"
 #include "plugin/PatternHelper.h"
 #include "utils/MemoryMgr.h"
+#include <Windows.h>
+#include <iostream>
 
 using namespace Memory::VP;
 
 
+static bool* bIsUsingTankControls = nullptr;
+static bool* bUnkCameraRelated = nullptr;
+static int   pContinueAddress = 0;
+
+void __declspec(naked) LookAroundHook()
+{
+    _asm {
+        movss   dword ptr[ebx + 0x28], xmm0
+    }
+    if (bUnkCameraRelated)
+         *bUnkCameraRelated = true;
+    _asm      jmp pContinueAddress
+}
+
 void Init()
 {
-    int togglePat = _pat("F3 0F 11 43 28 80 3D ? ? ? ? 00",11);
-    Patch<char>(togglePat, 1);
+    bIsUsingTankControls = nullptr;
+    bUnkCameraRelated = nullptr;
+    int tankControlsPat = _pat("EB 07 C6 05 ? ? ? ? 00 A1", 4);
+    bIsUsingTankControls = (bool*)(*(int*)tankControlsPat);
+
+    int unknownCameraPat = _pat("F3 0F 11 43 28 80 3D ? ? ? ? 00", 7);
+    bUnkCameraRelated = (bool*)(*(int*)unknownCameraPat);
+
+    int togglePat = _pat("F3 0F 11 43 28 80 3D ? ? ? ? 00");
+    pContinueAddress = togglePat + 5;
+
+
+
+    InjectHook(togglePat, LookAroundHook, PATCH_JUMP);
 }
 
 
